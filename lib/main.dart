1f1d641c405bfdf3759db051a85dfda9f3941f20
yuhyeon <dbgus3577@gmail.com>
2023-04-12 18:29:42 +0900

    BuildContext를 통한 Theme Text color 적용

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,104 @@ import 'package:flutter_example/widgets/button.dart';
 import 'package:flutter_example/widgets/currency_card.dart';
 
 void main() {
-  runApp(App());
+  // runApp(App());
+  runApp(const App3());
+}
+
+class App3 extends StatefulWidget {
+  const App3({Key? key}) : super(key: key);
+
+  @override
+  State<App3> createState() => _App3State();
+}
+
+class _App3State extends State<App3> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.red,
+          ),
+        ),
+      ),
+      home: Scaffold(
+        backgroundColor: const Color(0xFFF4EDDB),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              MyLargeTitle(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyLargeTitle extends StatelessWidget {
+  const MyLargeTitle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'My Large Title',
+      style: TextStyle(
+        fontSize: 30,
+        color: Theme.of(context).textTheme.titleLarge?.color,
+      ),
+    );
+  }
+}
+
+class App2 extends StatefulWidget {
+  const App2({Key? key}) : super(key: key);
+
+  @override
+  State<App2> createState() => _App2State();
+}
+
+class _App2State extends State<App2> {
+  List<int> numbers = [];
+
+  // int counter = 0;
+
+  void onClicked() {
+    setState(() {
+      numbers.add(numbers.length);
+      // counter = counter + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color(0xFFF4EDDB),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Click Counter',
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+              for (var n in numbers) Text('$n'),
+              IconButton(
+                iconSize: 40,
+                onPressed: onClicked,
+                icon: const Icon(Icons.add_box_rounded),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
 
 class App extends StatelessWidget {
